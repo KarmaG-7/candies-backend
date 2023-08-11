d7bed6b35967df7968104293d64642b0d731cb21
Karma S Ghale <karma.ghale1990@gmail.com>
2023-08-11 12:02:47 -0400

    worked on validation

--- controllers/candyController.js
+++ controllers/candyController.js
@@ -1,75 +1,89 @@
 //DEPENDENCIES
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { getAllCandies, getOneCandy, createCandy, deleteCandy, updateCandy } = require('../queries/candies');
+const {
+  getAllCandies,
+  getOneCandy,
+  createCandy,
+  deleteCandy,
+  updateCandy,
+} = require("../queries/candies");
 
-const { checkName, checkIsBoolean, validateURL } = require('../validations/validations');
+const {
+  checkName,
+  checkIsBoolean,
+  validateURL,
+  checkPrice,
+  checkRating,
+} = require("../validations/validations");
 
 //ROUTES
 
 //INDEX - GET ALL CANDIES
-router.get('/', async (req, res) => {
-    let result = await getAllCandies();
+router.get("/", async (req, res) => {
+  let result = await getAllCandies();
 
-    if(result.length === 0){
-        res.status(500).send('No candies were returned')
-    }
-    else{
-        res.send(result);
-    }
-    
+  if (result.length === 0) {
+    res.status(500).send("No candies were returned");
+  } else {
+    res.send(result);
+  }
 });
 
 //INDEX - GET ONE CANDY
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    let result = await getOneCandy(id);
+  let result = await getOneCandy(id);
 
-    if(Object.keys(result).length === 0){
-        res.status(500).send("We're sorry we couldn't find that candy")
-    }
-    else{
-        res.send(result[0]);
-    }
+  if (Object.keys(result).length === 0) {
+    res.status(500).send("We're sorry we couldn't find that candy");
+  } else {
+    res.send(result[0]);
+  }
 });
 
 //CREATE - CREATE CANDY
-router.post('/', checkName, checkIsBoolean, validateURL, async (req, res) => {
+router.post(
+  "/",
+  checkName,
+  checkIsBoolean,
+  validateURL,
+  checkPrice,
+  checkRating,
+
+  async (req, res) => {
     let createdCandy = await createCandy(req.body);
 
-    if(Object.keys(createdCandy).length === 0){
-        res.status(500).send('Could not create the candy')
+    if (Object.keys(createdCandy).length === 0) {
+      res.status(500).send("Could not create the candy");
+    } else {
+      res.send(createdCandy);
     }
-    else{
-        res.send(createdCandy);
-    }
-});
+  }
+);
 
 //DELETE - DELETE CANDY
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    let deletedCandy = await deleteCandy(id);
+  let deletedCandy = await deleteCandy(id);
 
-    if(Object.keys(deletedCandy).length === 0){
-        res.status(500).send('Could not delete the candy')
-    }
-    else{
-        res.send(deletedCandy);
-    }
+  if (Object.keys(deletedCandy).length === 0) {
+    res.status(500).send("Could not delete the candy");
+  } else {
+    res.send(deletedCandy);
+  }
 });
 
 //UPDATE - UPDATE CANDY
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
 
-    let updatedCandy = await updateCandy(id, req.body);
+  let updatedCandy = await updateCandy(id, req.body);
 
-    res.send(updatedCandy);
-})
-
-
+  res.send(updatedCandy);
+});
 
 module.exports = router;
--- validations/validations.js
+++ validations/validations.js
@@ -1,57 +1,54 @@
 const checkName = (req, res, next) => {
-    if(!req.body.candy_name){
-        res.status(500).send("Sorry you need to provide a candy name")
-    }
-    else{
-        next();
-    }
-}
+  if (!req.body.candy_name) {
+    res.status(500).send("Sorry you need to provide a candy name");
+  } else {
+    next();
+  }
+};
 
 const checkIsBoolean = (req, res, next) => {
-    if(typeof req.body.is_favorite !== "boolean"){
-        res.status(500).send("This must either be true or false boolean value");
-    }
-    else{
-        next();
-    }
-}
+  if (typeof req.body.is_favorite !== "boolean") {
+    res.status(500).send("This must either be true or false boolean value");
+  } else {
+    next();
+  }
+};
 
-// const checkPrice = (req, res, next) => {
-//     if(!(typeof req.body.price === 'number' && Number.isFinite(req.body.price))){
-//         res.status(500).send("Price must be a number");
-//     }
-//     else{
-//         next();
-//     }
-// }
+const checkPrice = (req, res, next) => {
+  if (!isNaN(req.body.price)) {
+    next();
+  } else {
+    res.status(500).send("Price must be a valid number");
+  }
+};
 
-// const checkRating = (req, res, next) => {
-//     if(typeof req.body.rating !== "number"){
-//         res.status(500).send("The rating must be a number")
-//     }
-//     else if(req.body.rating < 0 || req.body.rating > 10){
-//         res.status(500).send("The rating must be a whole number between 0 and 10")
-//     }
-//     else{
-//         next();
-//     }
-// }
+const checkRating = (req, res, next) => {
+  const rating = req.body.rating;
+
+  if (!isNaN(rating) && rating >= 0 && rating <= 10) {
+    next();
+  } else {
+    res.status(500).send("The rating must be a number between 0 and 10");
+  }
+};
 
 const validateURL = (req, res, next) => {
-    if (
-      req.body.candy_image.substring(0, 7) === "http://" ||
-      req.body.candy_image.substring(0, 8) === "https://"
-    ) {
-      return next();
-    } else {
-      res
-        .status(400)
-        .json({ error: "You forgot to start your image link with http:// or https://" });
-    }
-  };
+  if (
+    req.body.candy_image.substring(0, 7) === "http://" ||
+    req.body.candy_image.substring(0, 8) === "https://"
+  ) {
+    return next();
+  } else {
+    res.status(400).json({
+      error: "You forgot to start your image link with http:// or https://",
+    });
+  }
+};
 
 module.exports = {
-    checkName,
-    checkIsBoolean,
-    validateURL
-}
+  checkName,
+  checkIsBoolean,
+  validateURL,
+  checkPrice,
+  checkRating,
+};
